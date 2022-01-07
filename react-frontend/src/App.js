b1777d8ccc2618ae0a02a98b629383b957b95ba5
pauljflanagan <pflan414@gmail.com>
2022-01-07 10:06:44 -0500

    Added React frontend

---
+++ react-frontend/src/App.js
@@ -0,0 +1,12 @@
+//eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+import UserComponent from './components/UserComponent';
+
+export default function App() {
+  return (
+    <div className="App">
+      <UserComponent />
+    </div>
+  );
+}
